18a91eaee0b51679fc217e85f5c48b317ce612d6
57KayBel0 <makgaekabelo,otshabi.kb@gmail.com>
2025-01-22 02:10:42 -0800

    my files

--- backend_node/routes/authRoutes.js
+++ backend_node/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
-const jwt = reuire('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 // User login 
 router.post('/login', async (req, res) => {
