4b509e6a9f50174a5c7d7c54a5bb3351a9dce4b4
57KayBel0 <makgaekabelo,otshabi.kb@gmail.com>
2025-01-24 17:27:10 -0800

    update

--- backend_node/server.js
+++ backend_node/server.js
@@ -25,7 +25,7 @@ app.use('/api/auth', authRoutes);
 
 // Your routes here
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
 });
 
